d71cab0e69755e14a17a1fceb1c1d9bdc7f53b7c
NEFUstudent <study.svfu@gmail.com>
2018-12-20 17:26:30 +0900

    comething new

--- website/KrasilnikovHalyev/doctors.php
+++ website/KrasilnikovHalyev/doctors.php
@@ -15,7 +15,7 @@
 	$SQLquery = 'SELECT * FROM doctor';
 	$SQLresult = mysqli_query($link,$SQLquery);
 
-	printf('<table cellspacing=\' 1 \' border=\' 2 \'> %s',"\n");
+	printf('<table cellspacing=\' 0 \' border=\' 1 \'> %s',"\n");
 	printf('<TR> %s',"\n");
 	printf('	<TH>Family name</TH> %s',"\n");
 	printf('	<TH>Name</TH> %s',"\n");
@@ -26,7 +26,7 @@
 	while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
 	{
 		printf('<TR>');
-		printf('<TD> %s </TD> <TD> %s </TD> ',$result[1],$result[2]$result[5]);
+		printf('<TD> %s </TD> <TD> %s </TD> <TD> %s </TD> ',$result[1],$result[2]$result[4]);
 		printf('</TR> %s',"\n");
 	}
 	printf('</table> %s',"\n");
