b4b16e42caacc9d3e6d96cc4eab4d9b80f3170ac
NEFUstudent <study.svfu@gmail.com>
2018-12-20 16:19:15 +0900

    something new

--- website/KorkinAydyn/Payment_History.php
+++ website/KorkinAydyn/Payment_History.php
@@ -11,7 +11,7 @@ include('config.php');
 $link = mysqli_connect($server, $user, $password, $database) or die('Error! You suck!'.mysqli_connect_error());
 echo '<P>Successfully connected<\P>';
 
-$SQLquery = 'SELECT * FROM Arenda';
+$SQLquery = 'SELECT * FROM Payment_History';
 $SQLresult = mysqli_query($link,$SQLquery);
 printf('<table cellspacing=\' 0 \' border=\' 1 \'> %s',"\n");
 	printf('<TR> %s',"\n");
