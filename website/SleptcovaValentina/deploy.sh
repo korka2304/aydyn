9fdae90ff67dd3be16b5607034c275f56198f084
NEFUstudents <study.svfu@gmail.com>
2018-12-20 15:26:31 +0000

    something new

--- website/SleptcovaValentina/deploy.sh
+++ website/SleptcovaValentina/deploy.sh
@@ -2,6 +2,6 @@ cd ..\..
 git config --global user.name NEFUstudents
 git config --global user.email study.svfu@gmail.com
 git add -A
-git commit -a -m "something new"
+git commit -a -m 'something new'
 git pull origin master
 git push origin master
