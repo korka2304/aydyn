0c317ccf014a31bcfe65ef8b3e5f5da71b0e7061
NEFUstudent <study.svfu@gmail.com>
2018-12-24 17:02:18 +0900

    comething new

--- website/GurinovAital/add_patient_form_action.php
+++ website/GurinovAital/add_patient_form_action.php
@@ -1,6 +1,6 @@
 <?php
 include('config.php');	
-$link = mysqli_connect($server, $user, $password, $database)
+$link = mysqli_connect('10.14.129.132', 'GurinovAjtal', 'CB5LagBA','GurinovAjtalDB')
 	or die('Error: Unable to connect: ' . mysqli_connect_error());
 
 
