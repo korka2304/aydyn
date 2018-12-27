4084c0ccdd2488d0eb6cd75ded0365eab94b0670
korka2304 <46101213+korka2304@users.noreply.github.com>
2018-12-27 15:49:20 +0900

    Update and rename dd_author_form_action.php to add_author_form_action.php

--- website/KorkinAydyn/add_author_form_action.php
+++ website/KorkinAydyn/add_author_form_action.php
@@ -2,15 +2,15 @@
 include('config.php');	
 $link = mysqli_connect($server, $user, $password, $database)
 	or die('Error: Unable to connect: ' . mysqli_connect_error());
-$passport = mysqli_real_escape_string($link, $_POST['passport']);
+$passporto = mysqli_real_escape_string($link, $_POST['passporto']);
 $first_name = mysqli_real_escape_string($link, $_POST['first_name']);
 $family_name = mysqli_real_escape_string($link, $_POST['family_name']);
 // Ecape String
 // PhPMyAdmin // Sakila and World
-echo $passport
+echo $passporto
 echo $first_name;
 echo $family_name;
-INSERT INTO client (passport, FirstName, kianfouName) VALUES ((SELECT IFNULL(max(passport+1,1)) from (Select passport from authors) as ID), 
+INSERT INTO client (passport, FirstName, kianfouName) VALUES ((SELECT IFNULL(max($passporto+1,1)) from (Select passport from authors) as ID), 
 '$first_name','$family_name')
 echo '<BR> SQL query: ';
 echo $SQLquery;
