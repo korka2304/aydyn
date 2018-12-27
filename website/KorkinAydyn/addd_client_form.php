6084f25de91a03630b21e02ade7fe7198ea6a4a4
korka2304 <46101213+korka2304@users.noreply.github.com>
2018-12-27 15:21:45 +0900

    dobavlenie

--- website/KorkinAydyn/addd_client_form.php
+++ website/KorkinAydyn/addd_client_form.php
@@ -1,28 +1,49 @@
-<?php
-include('config.php');	
-$link = mysqli_connect($server, $user, $password, $database)
-	or die('Error: Unable to connect: ' . mysqli_connect_error());
+<html>
+ <head>
+  <title>Client add</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ </head>
+ <body>
+	<P style="color:red; font-size:18">
+	Welcome to the Korkin Aydyn realtor!<BR>
+	<BR>
+	Please feel free to zalozhit sosedey.
+	</P>
+ 	<meta charset="utf-8">
+	
+	<table width="100%" cellspacing="0" border="1">	
+		<TR>
+			<TH>Table column 1</TH>
+			<TH>Table column 2</TH>
+		</TR>
+		<TR>
+			<TD> 
+			<a href="arenda.php"> <P>Arenda</P> </a>
+			  <a href="Payment_History.php"> <P>Payment_History</P> </a>
+			  <a href="object.php"> <P>object1</P> </a>
+			  <a href="history.php"> <P>history</P> </a>                     
+			  <a href="client.php"> <P>client</P> </a>
+			  <a href="bank.php"> <P>bank</P> </a>
+			</TD>
+			<TD>
+			  <P>Add Author:</P>
+			  <form action="add_cliet_form_action.php" method="post">
+          		  	family name: <input type="text" name="family_name">
+          		  	<br>
+				Name: <input type="text" name="name">
+          		  	<br>
+				 kianfou name:<input type="text" name="kainfou name">
+				  <br>
+				  Pass: <input type="number" name="Pass">
+          		  	<br>
+				Adress: <input type="text" name="Adress">
+          		  	<br>
+				 Srok:<input type="text" name="srok">
+				  <br>
+            		  	<input type="submit" value="Add Author">
+      			  </form>
+			</TD>
+		</TR>
 
-
-$first_name = mysqli_real_escape_string($link, $_POST['first_name']);
-$family_name = mysqli_real_escape_string($link, $_POST['family_name']);
-// Ecape String
-
-// PhPMyAdmin // Sakila and World
-echo $first_name;
-echo $family_name;
-
-$SQLquery = "INSERT INTO client (AuthorID, FirstName, FamilyName) VALUES ((SELECT max(AuthorID)+1 from (Select AuthorID from authors) as ID), '$first_name','$family_name')";
-echo '<BR> SQL query: ';
-echo $SQLquery;
-
-if (mysqli_query($link, $SQLquery)) {
-    echo "<BR>New record created successfully";
-} else {
-    echo "<BR>Error: " . $sql . "<br>" . mysqli_error($link);
-}
-
-mysqli_close($link);
-
-printf('<a href="index.html"> <P>GO BACK</P> </a>');
-?>
+ </body>
+</html>
